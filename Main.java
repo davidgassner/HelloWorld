7fafde731c2cc6c7116d1bad1e7bcba038b71b81
David Gassner <david.gassner@gmail.com>
2022-04-15 22:15:12 +0000

    Changes

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("hello again!");
-        System.out.println("again");
-        System.out.println("more");
+        System.out.println("Hello, David!");
     }
 }
